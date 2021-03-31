c8f2c8103dfc9bf1d22b136242acacf491222a32
Ambar Dudhane <ambarsd12345@hotmail.com>
2021-03-31 10:57:15 -0500

    updated requirements

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
-import cv2
-import numpy as np
-import glob
-
-# for filename in glob.glob('temp/frames/*.png'):
-#     print(filename)
-
-
-# read image
-img = cv2.imread('temp/frames/frame0.png')
-
-# get dimensions of image
-dimensions = img.shape
-
-# height, width, number of channels in image
-height = img.shape[0]
-width = img.shape[1]
-channels = img.shape[2]
-
-print('Image Dimension    : ', dimensions)
-print('Image Height       : ', height)
-print('Image Width        : ', width)
-print('Number of Channels : ', channels)
-
+# import cv2
+# import numpy as np
+# import glob
+#
+# # for filename in glob.glob('temp/frames/*.png'):
+# #     print(filename)
+#
+#
+# # read image
+# img = cv2.imread('temp/frames/frame0.png')
+#
+# # get dimensions of image
+# dimensions = img.shape
+#
+# # height, width, number of channels in image
+# height = img.shape[0]
+# width = img.shape[1]
+# channels = img.shape[2]
+#
+# print('Image Dimension    : ', dimensions)
+# print('Image Height       : ', height)
+# print('Image Width        : ', width)
+# print('Number of Channels : ', channels)
+#
+import flask
+print(flask.__version__)
